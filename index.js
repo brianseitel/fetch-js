0081e638a7dfbeccf9c59e548a318ea57520304d
Brian Seitel <brianseitel@gmail.com>
2015-11-04 15:56:19 -0800

    feat(fetch): First commit!

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+exports.fetch = function (obj, key, _default) {
+  if (obj.hasOwnProperty(key)) {
+    return obj[key];
+  }
+
+  if (typeof key == 'object') {
+    var k = key.shift();
+    if (key.length > 0) {
+      return exports.fetch(obj[k], key);
+    } else {
+      return obj[k];
+    }
+  } else {
+    if (obj[key] != null) {
+      return obj[key];
+    }
+  }
+
+  return _default ? _default : null;
+}
